c0bb49e98977bcfc069cdc8ef22ec24a072b8e8b
Sung-hyoun <jqwesasd7703@naver.com>
2021-11-05 11:41:15 +0900

    내 정보 구현중

--- src/MyPage.js
+++ src/MyPage.js
@@ -1,23 +1,14 @@
 import {Redirect, Link} from "react-router-dom";
 
 function MyPage(){
-    function onClick(e){
-      
-        if(e.target.name === "profile"){
-            return(
-                <Redirect to="/profile"></Redirect>
-            )
-        }
-       
-    }
+
 
     return(
         <>
-            <div name="profile" onClick={onClick}>
+            <div name="profile">
                 <ul>
-                    <li>국비위키 프로필</li>
-                    <li><img src=""></img></li>
-                    <li>프로필 사진</li>
+                    <li><img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" style={{width:50, height:50}}></img></li>
+                    <li><Link to="http://localhost:3000/#/myPage/profile">국비위키 프로필</Link></li>
                     <li>
                         고객님의 프로필을 수정하실수
                                 있습니다.
@@ -27,26 +18,23 @@ function MyPage(){
 
             <div name="modify">
                 <ul>
-                    <li>개인정보 변경</li>
-                    <li><img src=""></img></li>
-                    <li>개인정보 이미지</li>
+                    <li><img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" style={{width:50, height:50}}></img></li>
+                    <li><Link to="http://localhost:3000/#/myPage/modify">개인정보 변경</Link></li>
                     <li>고객님의 개인정보를 수정하실 수 있습니다</li>
                 </ul>
             </div>
             <div name="remove">
                 <ul>
-                    <li>회원 탈퇴</li>
-                    <li><img src=""></img></li>
-                    <li>탈퇴 이미지</li>
+                    <li><img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" style={{width:50, height:50}}></img></li>
+                    <li><Link to="http://localhost:3000/#/myPage/remove">회원 탈퇴</Link></li>
                     <li>사용하지 않는 국비위키ID를 
                           탈퇴 할 수 있습니다.</li>
                 </ul>
             </div>
             <div name="passwordModify">
                 <ul>
-                    <li>비밀번호 변경</li>
-                    <li><img src=""></img></li>
-                    <li>비밀번호 이미지</li>
+                    <li><img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" style={{width:50, height:50}}></img></li>
+                    <li><Link to="http://localhost:3000/#/myPage/passwordModify">비밀번호 변경</Link></li>
                     <li>추가적인 비밀번호 변경으로
                          ID를 안전하게 관리할 수
                               있습니다.</li>
